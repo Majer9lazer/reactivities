72c98f29baa01913339ddf1f9e104475bbf71dff
Egor <sidorenkoegor1999@mail.ru>
2019-10-31 17:09:27 +0600

    End of section 5

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -50,7 +50,6 @@ namespace Application.Activities
 
                 _context.Add(activity);
 
-
                 var success = await _context.SaveChangesAsync(cancellationToken) > 0;
 
                 if (success)
